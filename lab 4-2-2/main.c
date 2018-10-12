e2211c60db77a7168c5b6747df711b49342e6e13
Olga <olgagekht@gmail.com>
2018-10-12 19:39:30 +0300

    Update lab 4-2-2/main.c

--- lab 4-2-2/main.c
+++ lab 4-2-2/main.c
@@ -1,15 +1,66 @@
 //
 //  main.c
-//  lab 4-2-2
-//
-//  Created by Оля on 12/10/2018.
-//  Copyright © 2018 Оля. All rights reserved.
+//В одномерном массиве, состоящем из n вещественных элементов, вычислить:
+//- номер максимального по модулю элемента массива;
+//- сумму элементов массива, расположенных после первого положительного элемента.
+
 //
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
+    
+    
+    
+    
+    
+    int N=1000, array[N], max=array[0], sum=0, a=0, number_max=0;
+    
+    printf("ENTER N (the number of the arrays' elements)\n");
+    scanf ("%d", &N);
+    
+    if (N>1000 || N<0)
+    {
+        printf("Enter another n (<1000)  \n");
+        scanf ("%d", &N);
+    }
+    
+    printf("The number of the array's elements %d\n", N);
+    
+    for (int i = 0; i<N; i++)                                  // input and output  of elements
+    {
+        printf("enter the element of the array ");
+        scanf("%d", &array[i]);
+    }
+    
+    for (int i=1; i<N; i++)                //dont work
+    {
+        if ( abs(array[i])>abs(max) )
+            max=array[i];
+        number_max=i;
+    }
+    
+    printf("max(abs) is  %d%d\n", max, number_max);
+    
+    
+    for (int i=0; i<N; i++)                //to find the 1st positive number
+    {
+        if (array[i]>0)
+        {a=i; break;}
+        
+    }
+    
+    for (int i=a; i<N; i++)                //sum
+    {
+        sum+=array[i];
+    }
+    
+    printf("sum is  %d\n", sum);
+    
     return 0;
 }
+
