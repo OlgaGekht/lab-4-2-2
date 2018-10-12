e8c99d96870191aa08e79f89e351dd31b75403c6
Оля <ola@macbook-air-ola.local>
2018-10-12 11:00:13 +0300

    Initial Commit

---
+++ lab 4-2-2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab 4-2-2
+//
+//  Created by Оля on 12/10/2018.
+//  Copyright © 2018 Оля. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
